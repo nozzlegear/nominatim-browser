477f86f005f05de584c6e4602c8c4e7dcc34efd7
Joshua Harms <joshua@nozzlegear.com>
2016-05-06 22:01:50 -0500

    Use Minneapolis as lookup address.

--- tests/lookup.ts
+++ tests/lookup.ts
@@ -11,11 +11,11 @@ describe(".lookupAddress", function ()
     it ("should return data for osm addresses", function (done)
     {
        lookup({
-           osm_ids: "R146656,W104393803,N240109189"
+           osm_ids: "R136712"
        })
        .then(function (results)
        {
-           expect(results.length).to.be.at.least(3);
+           expect(results.length).to.be.at.least(1);
            
             var result = results[0];
             
@@ -23,15 +23,15 @@ describe(".lookupAddress", function ()
             expect(result.licence).to.be.a("string");
             expect(result.lat).to.be.a("string");
             expect(result.lon).to.be.a("string");
-            expect(result.display_name).to.equal("Manchester, Greater Manchester, North West England, England, United Kingdom");
+            expect(result.display_name).to.equal("Minneapolis, Hennepin County, Minnesota, United States of America");
             expect(result.address).to.not.be.null;
             
             var address = result.address;
             
-            expect(address.city).to.equal("Manchester");
-            expect(address.county).to.equal("Greater Manchester");
-            expect(address.state).to.equal("England");
-            expect(address.country).to.equal("United Kingdom");
+            expect(address.city).to.equal("Minneapolis");
+            expect(address.county).to.equal("Hennepin County");
+            expect(address.state).to.equal("Minnesota");
+            expect(address.country).to.equal("United States of America");
             
             done();
        })
