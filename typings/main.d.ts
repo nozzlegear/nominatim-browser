7ef935455b12e86f458f61f15075f82c798c416f
Joshua Harms <joshua@nozzlegear.com>
2016-12-10 16:18:24 -0600

    Update to TypeScript 2.0, import definitions via @types packages

--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/definitions/bluebird/index.d.ts" />
-/// <reference path="main/definitions/reqwest/index.d.ts" />
